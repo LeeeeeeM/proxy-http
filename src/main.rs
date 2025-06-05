5501fa6a1b1730944038ab975b3a9d770ac97df8
xllgl2017@gmail.com <xinlanxi@gmail.com>
2025-06-05 16:46:07 +0800

    day5

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,24 @@
+mod cert;
+
 use std::error::Error;
+use std::io::BufReader;
 use std::net::{Ipv4Addr, SocketAddr, SocketAddrV4, ToSocketAddrs};
+use std::ptr::read;
+use std::sync::Arc;
+use rustls::{ClientConfig, RootCertStore};
+use rustls::server::ClientCertVerifierBuilder;
+use rustls_pemfile::Item;
+use rustls_pki_types::{DnsName, PrivateKeyDer, ServerName};
 use tokio::io::{AsyncReadExt, AsyncWriteExt};
 use tokio::net::{TcpListener, TcpStream};
+use tokio_rustls::rustls::{ServerConfig, SupportedProtocolVersion};
+use tokio_rustls::{TlsAcceptor, TlsConnector};
 
 #[tokio::main]
 async fn main() {
     // println!("{:?}"," ".as_bytes().to_vec());
-    // start_server().await.unwrap();
-    start_socks5_server().await.unwrap()
+    start_server().await.unwrap();
+    // start_socks5_server().await.unwrap()
 }
 
 
@@ -19,7 +30,7 @@ async fn start_socks5_server() -> Result<(), Box<dyn Error>> {
         println!("{}", addr);
         //启动一个线程，避免造成其他连接阻塞，影响网络体验
         tokio::spawn(async move {
-            handle_socks5_client(stream).await.unwrap();//Broken pipe这个是异常断开，就是我们的浏览器，突然关闭窗口了
+            handle_socks5_client(stream).await.unwrap(); //Broken pipe这个是异常断开，就是我们的浏览器，突然关闭窗口了
         });
     }
 }
@@ -75,7 +86,7 @@ async fn handle_socks5_client(mut inbound: TcpStream) -> Result<(), Box<dyn Erro
     };
     inbound.write(&[5, 0, 0, 1, 127, 0, 0, 1, 0, 80]).await?; //这里的127,0,0,1,0,80为地址127.0.0.1:80无意义
     //这里我们就完成了socks5代理的建立
-    copy_io(inbound,outbound).await?;
+    copy_io(inbound, outbound).await?;
     Ok(())
 }
 
@@ -112,7 +123,7 @@ async fn handle_client_stream(mut stream: TcpStream) -> Result<(), Box<dyn Error
     if buffer.starts_with(b"CONNECT") {
         handle_https(stream, buffer, len).await?;
     } else {
-        handle_http(stream, buffer, len).await?;
+        // handle_http(stream, buffer, len).await?;
     }
     Ok(())
 }
@@ -123,19 +134,23 @@ async fn handle_https(mut stream: TcpStream, buffer: [u8; 4096], len: usize) -> 
     if addr.len() == 0 { return Err("获取HTTPS真实地址失败".into()); }
     stream.write(b"HTTP/1.1 200 OK\r\n\r\n").await?;
     stream.flush().await?;
+    println!("{}", addr[0]);
     //从这里开始，两个stream之间交互的就是真实的https数据了
+    let sni = addr[0].split(":").next().unwrap();
+    let acceptor = gen_acceptor_for_sni(sni)?;
+    let inbound = acceptor.accept(stream).await?;
+    let mut root_ca = RootCertStore::empty();
+    root_ca.extend(webpki_roots::TLS_SERVER_ROOTS.iter().cloned());
+    let client_config = ClientConfig::builder().with_root_certificates(root_ca).with_no_client_auth();
     let outbound = TcpStream::connect(&addr[0]).await?;
-    copy_io(stream,outbound).await
-    // let (mut inbound_reader, mut inbound_writer) = tokio::io::split(stream);
-    // let (mut outbound_reader, mut outbound_writer) = tokio::io::split(outbound);
-    // let rt1 = tokio::spawn(async move {
-    //     let _ = tokio::io::copy(&mut inbound_reader, &mut outbound_writer).await;
-    // });
-    // let rt2 = tokio::spawn(async move {
-    //     let _ = tokio::io::copy(&mut outbound_reader, &mut inbound_writer).await; //这里报错是因为连接异常中断，这个我们的代理不用管
-    // });
-    // tokio::join!(rt1,rt2);
-    // Ok(())
+    let connector = TlsConnector::from(Arc::new(client_config));
+    println!("{}", sni);
+    let server_name = ServerName::DnsName(DnsName::try_from(sni.to_string())?);
+    let outbound = connector.connect(server_name, outbound).await?;
+    // //这里我们就实现了HTTPS解密，但是我们的根证书还没安装
+    // //sudo cp sca.pem /etc/pki/ca-trust/source/anchors/
+    // //sudo update-ca-trust
+    copy_io(inbound, outbound).await
 }
 
 async fn handle_http(stream: TcpStream, buffer: [u8; 4096], len: usize) -> Result<(), Box<dyn Error>> {
@@ -158,10 +173,14 @@ async fn handle_http(stream: TcpStream, buffer: [u8; 4096], len: usize) -> Resul
     // outbound.write(&buffer[..len]).await?;
     outbound.write(&buffer[..start_pos]).await?;
     outbound.write(&buffer[end_pos..len]).await?;
-    copy_io(stream,outbound).await
+    copy_io(stream, outbound).await
 }
 
-async fn copy_io(inbound:TcpStream,outbound:TcpStream)->Result<(),Box<dyn Error>> {
+async fn copy_io<I, O>(inbound: I, outbound: O) -> Result<(), Box<dyn Error>>
+where
+    I: AsyncReadExt + AsyncWriteExt + Send + Unpin + 'static,
+    O: AsyncReadExt + AsyncWriteExt + Send + Unpin + 'static,
+{
     let (mut inbound_reader, mut inbound_writer) = tokio::io::split(inbound);
     let (mut outbound_reader, mut outbound_writer) = tokio::io::split(outbound);
     let rt1 = tokio::spawn(async move {
@@ -186,4 +205,31 @@ fn regex_find(rex: &str, context: &str) -> Result<Vec<String>, Box<dyn Error>> {
         res.extend(r);
     };
     Ok(res)
+}
+
+
+//这里需要实现一个TlsAcceptor才能解密
+fn gen_acceptor_for_sni(sni: impl AsRef<str>) -> Result<TlsAcceptor, Box<dyn Error>> {
+    //这里先要生成证书
+    let (pem, key) = cert::gen_cert_for_sni(sni.as_ref(), "sca.pem", "sca.key")?;
+    let ca_bs = pem.into_bytes();
+    let key_bs = key.into_bytes();
+    let mut reader = BufReader::new(ca_bs.as_slice());
+    let item = rustls_pemfile::read_one(&mut reader).transpose().ok_or("读取证书失败")??;
+    let sni_cert = match item {
+        Item::X509Certificate(cert) => cert,
+        _ => return Err("不支持的证书".into()),
+    };
+    let mut reader = BufReader::new(key_bs.as_slice());
+    let item = rustls_pemfile::read_one(&mut reader).transpose().ok_or("读取证书密钥失败")??;
+    let sni_key = match item {
+        Item::Pkcs1Key(key) => PrivateKeyDer::Pkcs1(key),
+        Item::Pkcs8Key(key) => PrivateKeyDer::Pkcs8(key),
+        Item::Sec1Key(key) => PrivateKeyDer::Sec1(key),
+        _ => return Err("不支持的证书密钥类型".into()),
+    };
+    let config = ServerConfig::builder_with_protocol_versions(&rustls::ALL_VERSIONS)
+        .with_no_client_auth().with_single_cert(vec![sni_cert], sni_key)?;
+    let acceptor = TlsAcceptor::from(Arc::new(config));
+    Ok(acceptor)
 }
